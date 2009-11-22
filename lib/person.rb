973fe1fa715a332c8a46cf69e827fa984e4e836f
LukeHammond <luke.e.j.hammond@gmail.com>
2009-11-22 10:14:10 -0500

    Refactored compare method in person. Dropped old_compare which is obsolete. Brought lines of code down from 5 to 2, and avoided return in loop.

--- lib/person.rb
+++ lib/person.rb
@@ -10,18 +10,9 @@ class Person < Struct.new(:last_name, :first_name, :gender, :dob, :favorite_colo
     [self.last_name, self.first_name, self.gender, self.dob.strftime('%m/%d/%Y'), self.favorite_color].join(" ")
   end
 
-  def old_compare(other, sort = [:gender, :last_name])
-    significant_attr = sort.detect{|attr| self.send(attr) != other.send(attr)}
-    significant_attr ||= sort.first
-    self.send(significant_attr) <=> other.send(significant_attr)
-  end
-
   def compare(other, sort = [:gender, :last_name])
-      sort.each do |attr|
-        compare_value = self.send(attr) <=> other.send(attr)
-        return compare_value unless compare_value == 0
-      end
-      0
+    significant_attr = sort.detect{|attr| self.send(attr) != other.send(attr)}
+    significant_attr ? self.send(significant_attr) <=> other.send(significant_attr) : 0
   end
 
   def gender= value
